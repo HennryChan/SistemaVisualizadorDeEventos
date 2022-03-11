6447247760cf4b68f155fbee651c3fabe4a258d9
Hennry Chan <dual.hennry.chan@gmail.com>
2022-03-11 11:19:38 -0600

    Se agrego en Modelos GetSetEventos

---
+++ GetSetEventos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public string nombreArchivo { get; set; }
+
+		public string [] cadenaTxt { get; set; }
+
+		public string fechaHora { get; set; }
+		public string nombreEveto { get; set; }
+	
+		public TimeSpan diFecha { get; set; }
+		public string mensaje { get; set; }
+}
+}
