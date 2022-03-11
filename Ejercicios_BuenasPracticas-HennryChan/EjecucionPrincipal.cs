c0e68c538194bcb951864a680da6f14de8eb9ee8
Hennry Chan <dual.hennry.chan@gmail.com>
2022-03-11 10:42:56 -0600

    Agregaron cambios TestEventos en el proyecto

---
+++ Ejercicios_BuenasPracticas-HennryChan/EjecucionPrincipal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios_BuenasPracticas_HennryChan
+{
+    public class EjecucionPrincipal
+    {
+        public void InicioPrincipal()
+        {
+            LogicaEventos ObjLogicaEventos = new LogicaEventos();
+
+            Console.WriteLine("Sistema Visualizador de Eventos\n");
+            ObjLogicaEventos.LeerArchivosTxT();
+
+            Console.ReadKey();
+        }
+    }
+}
